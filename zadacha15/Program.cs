ed6840f3a9314012f9c64c856dbbc93077f7520e
Diana <miac.stat@mail.ru>
2022-09-20 15:27:36 +0300

    zadacha15 format

--- zadacha15/Program.cs
+++ zadacha15/Program.cs
@@ -2,27 +2,35 @@
 
 Console.Write("Введи цифру, обозначающую день недели: ");
 string num = Console.ReadLine();
-int dayNum = int.Parse (num);
+int dayNum = int.Parse(num);
 
-if (dayNum == 1) {
-Console.WriteLine("Понедельник - день тяжелый! И это не выходной.");
+if (dayNum == 1)
+{
+    Console.WriteLine("Понедельник - день тяжелый! И это не выходной.");
 }
-else if (dayNum == 2) {
-Console.WriteLine("Вторник - этот будний день... Крепись!");
+else if (dayNum == 2)
+{
+    Console.WriteLine("Вторник - этот будний день... Крепись!");
 }
-else if (dayNum == 3) {
-Console.WriteLine("Среда - этот будний день... Но, как говорится: среда - это маленькая пятница!");
+else if (dayNum == 3)
+{
+    Console.WriteLine("Среда - этот будний день... Но, как говорится: среда - это маленькая пятница!");
 }
-else if (dayNum == 4) {
-Console.WriteLine("Четверг этот будний день... Не время расслабляться!");
+else if (dayNum == 4)
+{
+    Console.WriteLine("Четверг этот будний день... Не время расслабляться!");
 }
-else if (dayNum == 5) {
-Console.WriteLine("Пятница - этот будний день... Крепись! Выходные не за горами!");
+else if (dayNum == 5)
+{
+    Console.WriteLine("Пятница - этот будний день... Крепись! Выходные не за горами!");
 }
-else if (dayNum == 6) {
-Console.WriteLine("Суббота - это выходной день! Можно погулять с друзьями.");
+else if (dayNum == 6)
+{
+    Console.WriteLine("Суббота - это выходной день! Можно погулять с друзьями.");
 }
-else if (dayNum == 7) {
-Console.WriteLine("Воскресенье - это выходной день! Завтра на работу, ложись пораньше.");
+else if (dayNum == 7)
+{
+    Console.WriteLine("Воскресенье - это выходной день! Завтра на работу, ложись пораньше.");
 }
-else Console.WriteLine("В неделе 7 дней ;)");
+else
+    Console.WriteLine("В неделе 7 дней ;)");
