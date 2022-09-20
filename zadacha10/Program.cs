07f8e1b19cc2aa627436458ffe56590946b562d2
Diana <miac.stat@mail.ru>
2022-09-20 15:27:00 +0300

    zadacha10 format

--- zadacha10/Program.cs
+++ zadacha10/Program.cs
@@ -3,6 +3,6 @@
 Console.WriteLine("Введите трехзначное число: ");
 string num = Console.ReadLine();
 
-int number = int.Parse (num); // если необходимо закрыть программу при вводе значения отличного от числа.
+int number = int.Parse(num); // если необходимо закрыть программу при вводе значения отличного от числа.
 
 Console.WriteLine($"Вторая цифра числа {num} - это {num[1]}.");
